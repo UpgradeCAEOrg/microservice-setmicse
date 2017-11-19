5900e8d3988ea1ffec03fab6e91429291bd8213f
mallika-mewar <MmNb1310>
2017-11-19 18:18:26 +0100

    Generated microservice version 1.0

---
+++ db/setmicse_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service setmicse, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table track.
+--
+CREATE TABLE commedit.track (
+  singer VARCHAR(255) ,
+  trackId VARCHAR(255) ,
+CONSTRAINT trackId_PK PRIMARY KEY (trackId)
+ 
+);
+
+
+
